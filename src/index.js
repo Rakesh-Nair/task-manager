c3112df06a32d32e71fbdefd720f12ae68de1292
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-19 12:36:07 +0530

    Added functionality to fetch a single user

--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ require('./db/mongoose');
 
 const User = require('./model/user');
 const Task = require('./model/task');
+const { ObjectID } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.status(200).send(user);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+});
 
 app.post('/users', (req, res) => {
     const user = new User(req.body);
@@ -18,6 +39,14 @@ app.post('/users', (req, res) => {
         res.status(400).send(error);
     })
 })
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.status(200).send(tasks);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+});
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
