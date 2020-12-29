946344e369c5d0634eeac5c7c42a3cacf9fa699a
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-29 21:47:39 +0530

    Added route files for User and Task

---
+++ src/routers/task.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = new express.Router();
+const Task = require('../model/task');
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).send(tasks);
+    }
+    catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.status(200).send(task);
+    }
+    catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save();
+        res.status(201).send(task);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
+
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['completed', 'description'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        return res.status(404).send({ 'error': 'Invalid updates !' });
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!task) {
+            return res.status(404).send({ 'error': 'No Task found to Update !' });
+        }
+
+        res.status(200).send(task);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    try {
+        if (!task) {
+            return res.status(400).send({ error: 'Task not found !' });
+        }
+        res.status(200).send();
+    }
+    catch (e) {
+        return res.status(500).send({ error: 'Unable to Delete !' });
+    }
+});
+
+module.exports = router;
---
+++ src/routers/user.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../model/user');
+
+router.get('/users', async (req, res) => {
+    const users = await User.find({});
+    try {
+        res.status(200).send(users);
+    }
+    catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+    try {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.status(200).send(user);
+    }
+    catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save()
+        res.status(201).send(user);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['age', 'name', 'email', 'password'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(404).send({ 'error': 'Invalid updates !' });
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!user) {
+            return res.status(404).send({ 'error': 'No User found to Update !' });
+        }
+
+        res.status(200).send(user);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/users/:id', async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    try {
+        if (!user) {
+            return res.status(400).send({ error: 'User not found !' });
+        }
+        res.status(200).send();
+    }
+    catch (e) {
+        return res.status(500).send({ error: 'Unable to Delete !' });
+    }
+});
+
+module.exports = router;
