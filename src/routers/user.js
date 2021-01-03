6b6fd33e112aeeb764b0f18b529803ff3f7d5efb
Rakesh Nair <Rakesh.Nair@accelya.local>
2021-01-03 12:15:32 +0530

    Updated patch and delete user functionality

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,20 +7,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    const user = await User.findById(_id);
-    try {
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user);
-    }
-    catch (e) {
-        res.status(500).send();
-    }
-});
-
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
@@ -69,41 +55,29 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 });
 
-router.patch('/users/:id', auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['age', 'name', 'email', 'password'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
     if (!isValidOperation) {
         return res.status(404).send({ 'error': 'Invalid updates !' });
     }
     try {
-        const user = await User.findById(req.params.id);
         updates.forEach((update) => {
-            user[update] = req.body[update];
+            req.user[update] = req.body[update];
         });
-
-        await user.save();
-        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-
-        if (!user) {
-            return res.status(404).send({ 'error': 'No User found to Update !' });
-        }
-
-        res.status(200).send(user);
+        await req.user.save();
+        res.status(200).send(req.user);
     }
     catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.delete('/users/:id', async (req, res) => {
-    const user = await User.findByIdAndDelete(req.params.id);
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        if (!user) {
-            return res.status(400).send({ error: 'User not found !' });
-        }
-        res.status(200).send();
+        await req.user.remove()
+        res.status(200).send(req.user);
     }
     catch (e) {
         return res.status(500).send({ error: 'Unable to Delete !' });
